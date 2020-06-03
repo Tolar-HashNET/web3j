2b8cdfa2278cf581f8a9b6e3036625f65fc91828
Bartol Bilić <bartol.bilic1@gmail.com>
2020-06-03 17:54:48 +0200

    remove senderAddressPassword

--- crypto/src/main/java/org/web3j/crypto/RawTransaction.java
+++ crypto/src/main/java/org/web3j/crypto/RawTransaction.java
@@ -14,29 +14,25 @@ package org.web3j.crypto;
 
 import java.math.BigInteger;
 
-import org.web3j.utils.Numeric;
-
 /**
  * Transaction class used for signing transactions locally.<br>
  * For the specification, refer to p4 of the <a href="http://gavwood.com/paper.pdf">yellow
  * paper</a>.
  */
 public class RawTransaction {
-
+    private static String CONTRACT_DEPLOY_ADDRESS = "54000000000000000000000000000000000000000023199e2b";
     private String senderAddress;
     private String receiverAddress;
     private BigInteger amount;
-    private String senderAddressPassword;
     private BigInteger gas;
     private BigInteger gasPrice;
     private String data;
     private BigInteger nonce;
 
-    protected RawTransaction(String senderAddress, String receiverAddress, BigInteger amount, String senderAddressPassword, BigInteger gas, BigInteger gasPrice, String data, BigInteger nonce) {
+    protected RawTransaction(String senderAddress, String receiverAddress, BigInteger amount, BigInteger gas, BigInteger gasPrice, String data, BigInteger nonce) {
         this.senderAddress = senderAddress;
         this.receiverAddress = receiverAddress;
         this.amount = amount;
-        this.senderAddressPassword = senderAddressPassword;
         this.gas = gas;
         this.gasPrice = gasPrice;
         this.data = data;
@@ -46,21 +42,19 @@ public class RawTransaction {
     public static RawTransaction createDeployContractTransaction(
             String senderAddress,
             BigInteger amount,
-            String senderAddressPassword,
             BigInteger gas,
             BigInteger gasPrice,
             String data,
             BigInteger nonce) {
 
         return new RawTransaction(
-                senderAddress, null, amount, senderAddressPassword, gas, gasPrice, data, nonce);
+                senderAddress, CONTRACT_DEPLOY_ADDRESS, amount, gas, gasPrice, data, nonce);
     }
 
     public static RawTransaction createFundTransferTransaction(
             String senderAddress,
             String receiverAddress,
             BigInteger amount,
-            String senderAddressPassword,
             BigInteger gas,
             BigInteger gasPrice,
             BigInteger nonce) {
@@ -69,33 +63,50 @@ public class RawTransaction {
                 senderAddress,
                 receiverAddress,
                 amount,
-                senderAddressPassword,
                 gas,
                 gasPrice,
-                null,
+                "",
                 nonce);
     }
 
-    public static RawTransaction createTransaction(String senderAddress, String receiverAddress, String senderAddressPassword, BigInteger gas, BigInteger gasPrice,
-            String data, BigInteger nonce) {
-        return createTransaction(senderAddress, receiverAddress, BigInteger.ZERO, senderAddressPassword, gas, gasPrice, data, nonce);
+    public static RawTransaction createExecuteFunctionTransaction(
+            String senderAddress,
+            String receiverAddress,
+            BigInteger amount,
+            BigInteger gas,
+            BigInteger gasPrice,
+            String data,
+            BigInteger nonce) {
+
+        return new RawTransaction(
+                senderAddress,
+                receiverAddress,
+                amount,
+                gas,
+                gasPrice,
+                data,
+                nonce);
+    }
+
+    public static RawTransaction createTransaction(String senderAddress, String receiverAddress, BigInteger gas, BigInteger gasPrice,
+                                                   String data, BigInteger nonce) {
+        return createTransaction(senderAddress, receiverAddress, BigInteger.ZERO, gas, gasPrice, data, nonce);
     }
 
     public static RawTransaction createTransaction(
             String senderAddress,
             String receiverAddress,
             BigInteger amount,
-            String senderAddressPassword,
             BigInteger gas,
             BigInteger gasPrice,
             String data,
             BigInteger nonce) {
 
-        return new RawTransaction(senderAddress, receiverAddress, amount, senderAddressPassword, gas, gasPrice, data, nonce);
+        return new RawTransaction(senderAddress, receiverAddress, amount, gas, gasPrice, data, nonce);
     }
 
     public static RawTransaction createTransaction(String receiverAddress, BigInteger amount, BigInteger gas, BigInteger gasPrice, String data, BigInteger nonce) {
-        return createTransaction(null, receiverAddress, amount, "", gas, gasPrice, data, nonce);
+        return createTransaction(null, receiverAddress, amount, gas, gasPrice, data, nonce);
     }
 
     public String getSenderAddress() {
@@ -108,10 +119,6 @@ public class RawTransaction {
 
     public BigInteger getAmount() {
         return amount;
-    }
-
-    public String getSenderAddressPassword() {
-        return senderAddressPassword;
     }
 
     public BigInteger getGas() {
