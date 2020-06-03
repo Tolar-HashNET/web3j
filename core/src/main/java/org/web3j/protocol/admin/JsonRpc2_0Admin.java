3bef2b9b1a23d6489210535369cc83754128bc79
Bartol Bilić <bartol.bilic1@gmail.com>
2020-06-03 02:04:28 +0200

    admin api is unsupported

--- core/src/main/java/org/web3j/protocol/admin/JsonRpc2_0Admin.java
+++ core/src/main/java/org/web3j/protocol/admin/JsonRpc2_0Admin.java
@@ -44,56 +44,30 @@ public class JsonRpc2_0Admin extends JsonRpc2_0Web3j implements Admin {
 
     @Override
     public Request<?, PersonalListAccounts> personalListAccounts() {
-        return new Request<>(
-                "personal_listAccounts",
-                Collections.<String>emptyList(),
-                web3jService,
-                PersonalListAccounts.class);
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Request<?, NewAccountIdentifier> personalNewAccount(String password) {
-        return new Request<>(
-                "personal_newAccount",
-                Arrays.asList(password),
-                web3jService,
-                NewAccountIdentifier.class);
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Request<?, PersonalUnlockAccount> personalUnlockAccount(
             String accountId, String password, BigInteger duration) {
-        List<Object> attributes = new ArrayList<>(3);
-        attributes.add(accountId);
-        attributes.add(password);
-
-        if (duration != null) {
-            // Parity has a bug where it won't support a duration
-            // See https://github.com/ethcore/parity/issues/1215
-            attributes.add(duration.longValue());
-        } else {
-            // we still need to include the null value, otherwise Parity rejects request
-            attributes.add(null);
-        }
-
-        return new Request<>(
-                "personal_unlockAccount", attributes, web3jService, PersonalUnlockAccount.class);
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Request<?, PersonalUnlockAccount> personalUnlockAccount(
             String accountId, String password) {
 
-        return personalUnlockAccount(accountId, password, null);
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Request<?, AccountSendRawTransaction> personalSendTransaction(
             Transaction transaction, String passphrase) {
-        return new Request<>(
-                "personal_sendTransaction",
-                Arrays.asList(transaction, passphrase),
-                web3jService,
-                AccountSendRawTransaction.class);
+        throw new UnsupportedOperationException();
     }
 }
